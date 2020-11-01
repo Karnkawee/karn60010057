8f71a97cd15c450d60abf368a7c7543005cd5a89
Karnkawee <60010057@kmitl.ac.th>
2020-11-01 14:22:06 +0700

    karn0057 master

--- stock/urls.py
+++ stock/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('reward', views.reward, name='reward'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
 
--- stock/views.py
+++ stock/views.py
@@ -6,10 +6,8 @@ def index(request):
     products = Product.objects.all()
     return render(request, 'frontend/index.html', {'products': products})
 
-
 def about(request):
     return render(request, 'frontend/about.html')
-
-
+    
 def contact(request):
     return render(request, 'frontend/contact.html')
